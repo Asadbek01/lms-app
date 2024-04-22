00360e143ac384652c79af566f0c24364e057869
Asadbek_Azamjonov <asadbek.azamjonov@devkom.pl>
2024-04-22 21:28:19 +0200

    initial commit

--- app/[lng]/api/webhook/route.ts
+++ app/[lng]/api/webhook/route.ts
@@ -3,6 +3,7 @@ import { sendNotification } from '@/actions/notification.action'
 import { createUser, updateUser } from '@/actions/user.action'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { Webhook } from 'svix'
+import { headers } from 'next/headers'
 
 export async function POST(req: Request) {
 	const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
@@ -13,6 +14,19 @@ export async function POST(req: Request) {
 		)
 	}
 
+	// Get the headers
+	const headerPayload = headers()
+	const svix_id = headerPayload.get('svix-id')
+	const svix_timestamp = headerPayload.get('svix-timestamp')
+	const svix_signature = headerPayload.get('svix-signature')
+
+	// If there are no headers, error out
+	if (!svix_id || !svix_timestamp || !svix_signature) {
+		return new Response('Error occurred -- no svix headers', {
+			status: 400,
+		})
+	}
+
 	const payload = await req.json()
 	const body = JSON.stringify(payload)
 
@@ -21,15 +35,23 @@ export async function POST(req: Request) {
 	let evt: WebhookEvent
 
 	try {
-		evt = wh.parse(body) as WebhookEvent
+		evt = wh.verify(body, {
+			'svix-id': svix_id,
+			'svix-timestamp': svix_timestamp,
+			'svix-signature': svix_signature,
+		}) as WebhookEvent
 	} catch (err) {
-		console.error('Error parsing webhook:', err)
-		return new Response('Error occurred', {
+		console.error('Error verifying webhook:', err)
+		return new Response('Error occured', {
 			status: 400,
 		})
 	}
-
+	// Get the ID and type
+	const { id } = evt.data
 	const eventType = evt.type
+
+	console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
+	console.log('Webhook body:', body)
 
 	if (eventType === 'user.created') {
 		const { id, email_addresses, image_url, first_name, last_name } = evt.data
